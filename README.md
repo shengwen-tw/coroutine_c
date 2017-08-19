b95b3465ce3cad0ca06123edabc90edec23745a3
Shengwen Cheng <shengwen1997.tw@gmail.com>
2017-08-19 21:11:52 +0800

    Pratice coroutine in C

---
+++ README.md
@@ -0,0 +1,11 @@
+#coroutine_c
+
+**usage:**
+
+```
+make
+./coroutine
+```
+
+* [reference](http://blog.linux.org.tw/~jserv/archives/001848.html) by Jim Huang (jserv.tw@gmail.com)
+
---
+++ main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#define cr_start() \
+	static int run_line = 0; \
+	switch(run_line) { \
+	case 0: \
+
+#define cr_end() \
+	} \
+	run_line = 0
+
+#define cr_yield() \
+	usleep(500000); \
+	run_line = __LINE__; \
+	return; \
+	case __LINE__:
+
+void routine1(void)
+{
+	cr_start();
+
+	while(1) {
+		printf("hello1\n");
+		cr_yield();
+		printf("hello2\n");
+		cr_yield();
+	}
+
+	cr_end();
+}
+
+void routine2(void)
+{
+	cr_start();
+
+	while(1) {
+		printf("hello3\n");
+		cr_yield();
+	}
+
+	cr_end();
+}
+
+void main(void)
+{
+	while(1) {
+		routine1();
+		routine2();
+	}
+
+	return 0;
+}
